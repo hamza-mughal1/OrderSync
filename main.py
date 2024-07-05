6ad2066f2d79cc23d3197fdabdb9efef11679b4e
hamza_mughal <96692951+hamza-mughal1@users.noreply.github.com>
2024-07-05 22:16:43 +0500

    main

--- main.py
+++ main.py
@@ -12,8 +12,8 @@ class Database:
 
         self.mycursor = self.db.cursor()
 
-    def all_products(self):
-        self.mycursor.execute("select * from products")
+    def all_products(self, limit = 1000, offset = 0):
+        self.mycursor.execute(f"SELECT * FROM PRODUCTS LIMIT {limit} OFFSET {offset}")
         return [list(i) for i in self.mycursor]
     
     def place_order(self, products, quantities):
@@ -21,9 +21,9 @@ class Database:
         total_price = 0 
         for pos, i in enumerate(products):
             self.mycursor.execute(f"SELECT ID, PRICE FROM PRODUCTS WHERE NAME = '{i}'")
-            ResultCursor = list(self.mycursor)[0]
-            insert_to_sales_detail.append([ResultCursor[0], quantities[pos],ResultCursor[1]*quantities[pos]])
-            total_price += ResultCursor[1]*quantities[pos]
+            result_cursor = list(self.mycursor)[0]
+            insert_to_sales_detail.append([result_cursor[0], quantities[pos],result_cursor[1]*quantities[pos]])
+            total_price += result_cursor[1]*quantities[pos]
 
         self.mycursor.execute(f"INSERT INTO SALES (total_amount) value ({total_price})")
         self.db.commit()
@@ -35,6 +35,22 @@ class Database:
         
         self.db.commit()
 
+    def all_sales(self):
+        sales_list = []
+        self.mycursor.execute("SELECT * FROM SALES")
+        for i in list(self.mycursor):
+            temp_ls = []
+            date = i[1]
+            price = float(i[2])
+            self.mycursor.execute(f"SELECT * FROM SALES_DETAIL WHERE SALE_ID = {i[0]}")
+            for j in list(self.mycursor):
+                self.mycursor.execute(f"SELECT name, price FROM PRODUCTS WHERE ID = {j[2]}")
+                result_cursor = list(self.mycursor)[0]
+                temp_ls.append((result_cursor[0],j[3],result_cursor[1],j[4]))
+
+            sales_list.append([temp_ls,date,price])
+
+        return sales_list
 
 
 mydb = Database(
@@ -46,4 +62,7 @@ mydb = Database(
 
 # print(mydb.all_products())
 
-mydb.place_order(["Margherita pizza large"],[5])
+# mydb.place_order(["Classic Hot Dog", "Ham Sandwich"],[2, 1])
+
+for i in mydb.all_sales():
+    print(i)
