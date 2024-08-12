3fa0984113db5e813cad780ff24797a8857cd51c
hamza_mughal <hamzaxd1123@gmail.com>
2024-08-12 17:11:11 +0500

    .

--- model/ordersync.sql
+++ model/ordersync.sql
@@ -3,7 +3,7 @@ USE ordersync;
 CREATE TABLE categories (
     id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(100) NOT NULL,
-    toggle TINYINT DEFAULT (1),
+    toggle TINYINT DEFAULT 1,
     PRIMARY KEY (id)
 );
 
@@ -12,8 +12,8 @@ CREATE TABLE products (
     category_id INT NOT NULL,
     name VARCHAR(200) NOT NULL UNIQUE,
     price INT NOT NULL,
-    toggle TINYINT DEFAULT (1),
-    image_path VARCHAR(300) DEFAULT ("None"),
+    toggle TINYINT DEFAULT 1,
+    image_path VARCHAR(300) DEFAULT "None",
     PRIMARY KEY (id),
     FOREIGN KEY (category_id) REFERENCES categories(id)
 );
@@ -30,17 +30,17 @@ CREATE TABLE users (
     password VARCHAR(300) NOT NULL,
     user_role INT NOT NULL,
     PRIMARY KEY(id),
-    FOREIGN KEY(user_role) REFERENCES roles(id)
+    FOREIGN KEY(user_role) REFERENCES role(id)
 );
 
 CREATE TABLE sales (
     id INT NOT NULL AUTO_INCREMENT,
     user_id INT NOT NULL,
     price INT NOT NULL,
-    date DATETIME DEFAULT (NOW()),
-    discount_per INT DEFAULT(0),
-    discount_price INT DEFAULT (0),
-    discount_des VARCHAR(500) DEFAULT ("None"),
+    date DATETIME DEFAULT NOW(),
+    discount_per INT DEFAULT 0,
+    discount_price INT DEFAULT 0,
+    discount_des VARCHAR(500) DEFAULT "None",
     PRIMARY KEY(id),
     FOREIGN KEY(user_id) REFERENCES users(id)
 );
@@ -49,11 +49,11 @@ CREATE TABLE sale_details (
     id INT NOT NULL AUTO_INCREMENT,
     sale_id INT NOT NULL,
     product_id INT NOT NULL,
-    quantity INT DEFAULT(1),
+    quantity INT DEFAULT 1,
     price INT NOT NULL,
-    discount_per INT DEFAULT(0),
-    discount_price INT DEFAULT (0),
-    discount_des VARCHAR(500) DEFAULT ("None"),
+    discount_per INT DEFAULT 0,
+    discount_price INT DEFAULT 0,
+    discount_des VARCHAR(500) DEFAULT "None",
     PRIMARY KEY(id),
     FOREIGN KEY (sale_id) REFERENCES sales(id),
     FOREIGN KEY (product_id)
@@ -66,7 +66,7 @@ CREATE TABLE available_refresh_token (
     user_name VARCHAR(255) NOT NULL,
     user_id INT NOT NULL, 
     user_role INT NOT NULL,
-    created_at TIMESTAMP DEFAULT (CURRENT_TIMESTAMP()),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP(),
     jwt_token VARCHAR(300) NOT NULL,
     PRIMARY KEY(id),
     FOREIGN KEY (user_id) REFERENCES users(id),
@@ -78,7 +78,7 @@ CREATE TABLE blacklisted_token (
     token VARCHAR(300) NOT NULL,
     user_id INT NOT NULL, 
     role INT NOT NULL,
-    created_at TIMESTAMP DEFAULT (CURRENT_TIMESTAMP()),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP(),
     PRIMARY KEY(id),
     FOREIGN KEY (user_id) REFERENCES users(id),
     FOREIGN KEY (role) REFERENCES roles(id)
@@ -87,10 +87,12 @@ CREATE TABLE blacklisted_token (
 CREATE TABLE endpoints (
     id INT NOT NULL AUTO_INCREMENT,
     endpoint VARCHAR(300),
-    method SET("GET","POST","PUT","PATCH","DELETE") DEFAULT ("GET"),
-    role JSON DEFAULT ("[]"),
+    method SET("GET","POST","PUT","PATCH","DELETE") DEFAULT "GET",
+    role JSON DEFAULT "[]",
     PRIMARY KEY(id)
 );
+
+SET GLOBAL event_scheduler = ON;
 
 CREATE EVENT delete_old_acess_tokens
 ON SCHEDULE EVERY 30 MINUTE
