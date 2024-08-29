b0f58a4f15db33b2db8443ac87e0db1e15a55215
hamza_mughal <hamzaxd1123@gmail.com>
2024-08-29 19:57:38 +0500

    add new endpoints and token authentication

--- handler/admin_handler.py
+++ handler/admin_handler.py
@@ -10,6 +10,7 @@ auth_model = AuthModel()
 
 # Route to handle the creation of a new endpoint
 @app.route("/admin/endpoints", methods=["POST"])
+@auth_model.token_auth()
 def create_endpoint():
     # Create a new endpoint using data from the request
     return admin_model.create_endpoint(request.json)
@@ -17,6 +18,7 @@ def create_endpoint():
 
 # Route to handle updating an existing endpoint
 @app.route("/admin/endpoints", methods=["PUT"])
+@auth_model.token_auth()
 def update_endpoint():
     # Update an existing endpoint using data from the request
     return admin_model.update_endpoint(request.json)
@@ -24,6 +26,7 @@ def update_endpoint():
 
 # Route to handle deletion of an endpoint
 @app.route("/admin/endpoints", methods=["DELETE"])
+@auth_model.token_auth()
 def delete_endpoint():
     # Delete an endpoint using data from the request
     return admin_model.delete_endpoint(request.json)
@@ -31,6 +34,7 @@ def delete_endpoint():
 
 # Route to handle the creation of a new role
 @app.route("/admin/roles", methods=["POST"])
+@auth_model.token_auth()
 def create_role():
     # Create a new role using data from the request
     return admin_model.create_role(request.json)
@@ -38,6 +42,7 @@ def create_role():
 
 # Route to handle updating an existing role
 @app.route("/admin/roles", methods=["PUT"])
+@auth_model.token_auth()
 def update_role():
     # Update an existing role using data from the request
     return admin_model.update_role(request.json)
@@ -45,6 +50,18 @@ def update_role():
 
 # Route to handle deletion of a role
 @app.route("/admin/roles", methods=["DELETE"])
+@auth_model.token_auth()
 def delete_role():
     # Delete a role using data from the request
     return admin_model.delete_role(request.json)
+
+@app.route("/admin/categories", method=["POST"])
+@auth_model.token_auth()
+def create_category(): 
+    return admin_model.create_category(request.json)
+
+
+@app.route("/admin/categories", method=["DELETE"])
+@auth_model.token_auth()
+def delete_category():
+    return admin_model.delete_category(request.json)
